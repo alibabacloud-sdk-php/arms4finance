cf8e7cd5353c6ecb2578f5dbc852f4ede7adb599
宋神宗 <zhijiang.nzj@alibaba-inc.com>
2019-05-09 16:23:46 +0800

    Generate composer.json for split (#114)

--- ARMS4FINANCE.php
+++ ARMS4FINANCE.php
@@ -5,8 +5,8 @@ namespace AlibabaCloud\ARMS4FINANCE;
 use AlibabaCloud\Client\Resolver\VersionResolver;
 
 /**
- * @deprecated
+ * @method static V20171130\ARMS4FINANCEApiResolver v20171130()
  */
-class ARMS4FINANCEVersion extends VersionResolver
+class ARMS4FINANCE extends VersionResolver
 {
 }
